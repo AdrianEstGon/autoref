ced373cd8628668f2548aae578df3248e1688c09
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-06-24 00:50:50 +0200

    grok despliegue

--- webapp/src/config.ts
+++ webapp/src/config.ts
@@ -1,3 +1,3 @@
-const API_URL = 'https://7b5ec380-243e-46df-b58c-27bb9bd7b812-dev.e1-eu-north-azure.choreoapis.dev/autoref/backend/v1.0';
+const API_URL = 'https://941a-83-54-69-202.ngrok-free.app/api';
 
 export default API_URL;
